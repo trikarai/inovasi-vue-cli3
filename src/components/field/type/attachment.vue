f7ea70a11b2c9a12a0063abd0c0c28e6784a45d3
Tri S <mas.trisutrisno@gmail.com>
2019-07-19 15:49:01 +0700

    field attachment upload stable
    - upload function ok
    
    //TODO
    - display on edit

--- src/components/field/type/attachment.vue
+++ src/components/field/type/attachment.vue
@@ -1,40 +1,102 @@
 <template>
   <div>
     <!-- https://jsfiddle.net/meyubaraj/fLbe7r72/ -->
-    <v-flex xs12 class="text-xs-center text-sm-center text-md-center text-lg-center" pt-3>
-      <img :src="imageUrl" height="150" v-if="imageUrl" />
+    <v-flex xs12 class="text-xs-center text-sm-center text-md-center text-lg-center">
+      <notification-alert v-bind:err_msg="err_msg" v-bind:status="status" />
+      <!-- <v-progress-linear v-if="progressShow" color="red" v-model="valueDeterminate"></v-progress-linear> -->
+      <v-expand-x-transition>
+        <v-progress-circular
+          v-if="progressShow"
+          rotate="90"
+          size="150"
+          width="10"
+          :value="valueDeterminate"
+          color="red"
+        >{{ valueDeterminate }} %</v-progress-circular>
+      </v-expand-x-transition>
+      <v-expand-transition>
+        <template v-if="!progressShow">
+          <img :src="imageUrl" height="150" v-if="imageUrl" />
+        </template>
+      </v-expand-transition>
+      <template v-if="!uploaded">
+        <v-btn
+          @click.prevent="uploadFile"
+          v-if="imageUrl"
+          :disabled="progressShow"
+          small
+          fab
+          color="red"
+        >
+          <v-icon>cloud_upload</v-icon>
+        </v-btn>
+      </template>
+      <v-btn fab small color="green" v-if="uploaded">
+        <v-icon>check</v-icon>
+      </v-btn>
       <v-text-field
-        :label="field.name"
+        label="Select Image"
         @click="pickFile"
         v-model="imageName"
         prepend-icon="attach_file"
       ></v-text-field>
+      <v-text-field style="display: none" label="Path" v-model="value"></v-text-field>
       <input type="file" style="display: none" ref="image" accept="image/*" @change="onFilePicked" />
     </v-flex>
   </div>
 </template>
 <script>
+import bus from "@/bus";
+import * as config from "@/config/config";
+import auth from "@/config/auth";
+import Notification from "@/components/Notification";
+import { uuid } from "vue-uuid";
+import notif from "@/config/alerthandling";
+
 export default {
   props: ["field"],
   components: {},
   data: function() {
     return {
+      status: {
+        success: false,
+        error: false,
+        info: false,
+        warning: false
+      },
+      err_msg: { code: 666, type: "", details: [] },
+      clearable: true,
+      readonly: true,
+      valueDeterminate: 0,
+      value: "ID IMAGE",
       imageName: "",
       imageUrl: "",
-      imageFile: ""
+      imageFile: "",
+      headers: {},
+      progressShow: false,
+      uploaded: false
     };
   },
-  watch: {},
+  components: {
+    "notification-alert": Notification
+  },
+  watch: {
+    value: function() {
+      var params = { id: this.field.id, value: this.value };
+      bus.$emit("getValue", params, this.field.position - 1);
+    }
+  },
   mounted() {},
   methods: {
     pickFile() {
       this.$refs.image.click();
     },
-
     onFilePicked(e) {
+      this.progressShow = false;
       const files = e.target.files;
       if (files[0] !== undefined) {
         this.imageName = files[0].name;
+        this.headers["file-name"] = this.$uuid.v4() + "_" + files[0].name;
         if (this.imageName.lastIndexOf(".") <= 0) {
           return;
         }
@@ -49,13 +111,46 @@ export default {
         this.imageFile = "";
         this.imageUrl = "";
       }
+    },
+    uploadFile: function() {
+      this.progressShow = true;
+      let data = JSON.parse(window.localStorage.getItem('lbUser'))
+      this.headers["Content-Type"] = "image/*";
+      this.headers["Authorization"] = "Bearer " + data.token;
+      var app = this;
+      this.$http
+        .post(
+          config.APIENDPOINT +
+            "/talent/as-team-member/" +
+            this.$route.params.teamId +
+            "/file-upload",
+          this.imageFile,
+          {
+            headers: this.headers,
+            progress(e) {
+              if (e.lengthComputable) {
+                console.log((e.loaded / e.total) * 100);
+                app.valueDeterminate = Math.round((e.loaded / e.total) * 100);
+              }
+            }
+          }
+        )
+        .then(res => {
+          console.log(res);
+          this.value = res.data.data.fileInfo.id;
+          this.uploaded = true;
+        })
+        .catch(error => {
+          console.log(error);
+          notif.showError(this, error);
+          this.uploaded = false;
+        })
+        .finally(() => {
+          this.progressShow = false;
+        });
     }
   }
 };
 </script>
 <style scoped>
-/* input[type="file"] {
-  position: absolute;
-  left: -99999px;
-} */
 </style>
