589f98ad9029972bcd96e9a079ee1f1b4c5e0605
Tri S <mas.trisutrisno@gmail.com>
2019-07-29 14:02:04 +0700

    Update text.vue

--- src/components/field/type/text.vue
+++ src/components/field/type/text.vue
@@ -1,54 +1,39 @@
 <template>
   <div>
     <v-flex xs12 sm12>
-      <tiptap-vuetify v-model="value" :placeholder="field.name" :extensions="extensions" />
-    </v-flex>
-    <!-- <v-flex xs12 sm12>
       <v-textarea
+        v-model="value"
+        :id="field.id"
+        :name="field.id"
         :label="field.name"
         :hint="field.description"
-        :counter="field.maxValue"
-        :maxlength="field.maxValue"
-        :rules="rules"
-        clearable="true"
-        single-line
+        counter
+        :clearable="clearable"
       ></v-textarea>
-    </v-flex>-->
+    </v-flex>
   </div>
 </template>
 <script>
 import bus from "@/bus";
-import { TiptapVuetify, OrderedList, ListItem, History } from "tiptap-vuetify";
 
 export default {
   props: ["field", "index"],
-  components: {
-    TiptapVuetify
-  },
+  components: {},
   data: function() {
     return {
+      clearable: true,
       value: "",
       rules: [
-        v => !!v || "This field is required",
-        v =>
-          v.length >= this.field.minValue ||
-          "Min " + this.field.minValue + " characters",
-        v =>
-          v.length <= this.field.maxValue ||
-          "Max " + this.field.maxValue + " characters"
-      ],
-      extensions: [
-        // you can specify options for extension
-        new OrderedList(),
-        new ListItem(),
-        new History()
+        v => !!v || "This field is required"
+        // v => v.length >= this.field.minValue || "Min " + this.field.minValue + " characters",
+        // v => v.length <= this.field.maxValue || "Max " + this.field.maxValue + " characters"
       ]
     };
   },
   watch: {
-    value: function(){
-      var params = { id: this.field.id , value: this.value }
-      bus.$emit("getValue", params, this.index)
+    value: function() {
+      var params = { id: this.field.id, value: this.value };
+      bus.$emit("getValue", params, this.index);
     }
   }
 };
