0547b50436c31e9a9ab3ef769b055f8caf5cc0f2
Tri S <mas.trisutrisno@gmail.com>
2019-08-09 16:36:26 +0700

    Update TalentNavBar.vue

--- src/components/talent/TalentNavBar.vue
+++ src/components/talent/TalentNavBar.vue
@@ -2,96 +2,114 @@
   <nav>
     <v-toolbar height="50" flat app color="primary">
       <v-toolbar-side-icon class="white--text" @click="drawer = !drawer"></v-toolbar-side-icon>
-      <!-- <v-toolbar fixed app :clipped-left="clipped" dark color="primary">
-        <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
-        <v-btn icon @click.stop="miniVariant = !miniVariant">
-          <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-        </v-btn>
-      </v-toolbar>-->
-      <!-- <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>-->
-      <!-- <v-toolbar-title class="text-uppercase white--text">
-        <span class="font-weight-light">START</span>
-        <span class>Mikti</span>
-      </v-toolbar-title> -->
       <v-spacer></v-spacer>
-      <!-- <v-btn flat @click="logout" v-if="checkLogin">
-        <span>{{ $vuetify.t('$vuetify.action.signout') }}</span>
-        <v-icon right>exit_to_app</v-icon>
-      </v-btn>-->
-
-        <v-icon @click="rightDrawer =! rightDrawer" color="white">settings</v-icon>
-
+      <v-icon @click="rightDrawer =! rightDrawer" color="white">settings</v-icon>
     </v-toolbar>
-
     <div>
       <v-breadcrumbs :items="$store.state.breads.item" divider=" > ">
         <template v-slot:item="props">
-          <router-link :to="props.item.path" :class="{disabled : props.item.disabled}">{{props.item.text}}</router-link>
+          <router-link
+            :to="props.item.path"
+            :class="{disabled : props.item.disabled}"
+          >{{props.item.text}}</router-link>
         </template>
       </v-breadcrumbs>
     </div>
     <!-- Vuex pos : {{$store.state.breads.pos}} | path level : {{$route.meta.level}} -->
-    <v-navigation-drawer width="270" id="navdrawtalent" app v-model="drawer" :mini-variant.sync="miniVariant">
-      <!-- list head-->
-      <v-list class="pa-1">
-        <v-list-tile v-if="miniVariant" @click.stop="miniVariant = !miniVariant">
-          <v-list-tile-action>
-            <v-icon>chevron_right</v-icon>
-          </v-list-tile-action>
-        </v-list-tile>
-        <v-list-tile avatar>
-          <v-list-tile-avatar>
-            <img src="/img/avatar.png" />
-          </v-list-tile-avatar>
-          <v-list-tile-content>
-            <v-list-tile-title>{{user.data.name}}</v-list-tile-title>
-          </v-list-tile-content>
-          <v-list-tile-action>
-            <v-btn icon @click.stop="miniVariant = !miniVariant">
-              <v-icon>chevron_left</v-icon>
-            </v-btn>
-          </v-list-tile-action>
-        </v-list-tile>
-      </v-list>
-      <!-- link router -->
-      <v-list>
-        <!--dashboar-->
-        <v-list-tile ripple router to="/talent/dashboard">
-          <v-list-tile-action>
-            <v-icon color="#676767">dashboard</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title class="grey--text">Dashboard</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-        <!-- profile-->
-        <v-list-group prepend-icon="account_circle" :value="group" no-action>
-          <template v-slot:activator>
-            <v-list-tile ripple>
-              <v-list-tile-title>Profile</v-list-tile-title>
-            </v-list-tile>
-          </template>
-          <template v-if="!miniVariant">
-            <v-list-tile ripple v-for="link in linkProfile" :key="link.text" router :to="link.route">
-              <v-list-tile-title>{{link.text}}</v-list-tile-title>
-              <v-list-tile-action>
-                <v-icon>{{link.icon}}</v-icon>
-              </v-list-tile-action>
-            </v-list-tile>
-          </template>
-        </v-list-group>
-        <!--sub list other-->
-        <v-list-tile ripple v-for="link in links" :key="link.text" router :to="link.route">
-          <v-list-tile-action>
-            <v-icon color="#676767">{{link.icon}}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title class="grey--text">{{link.text}}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
+    <v-navigation-drawer
+      width="270"
+      id="navdrawtalent"
+      app
+      v-model="drawer"
+      :mini-variant.sync="miniVariant"
+    >
+      <v-layout column justify-space-between fill-height>
+        <!-- list head-->
+        <v-list class="pa-1">
+          <v-list-tile v-if="miniVariant" @click.stop="miniVariant = !miniVariant">
+            <v-list-tile-action>
+              <v-icon>chevron_right</v-icon>
+            </v-list-tile-action>
+          </v-list-tile>
+          <v-list-tile avatar>
+            <v-list-tile-avatar>
+              <img src="/img/avatar.png" />
+            </v-list-tile-avatar>
+            <v-list-tile-content>
+              <v-list-tile-title>{{user.data.name}}</v-list-tile-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn icon @click.stop="miniVariant = !miniVariant">
+                <v-icon>chevron_left</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+        <!-- link router -->
+        <v-list>
+          <!--dashboar-->
+          <v-list-tile ripple router to="/talent/dashboard">
+            <v-list-tile-action>
+              <v-icon color="#676767">dashboard</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title class="grey--text">Dashboard</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <!-- profile-->
+          <v-list-group prepend-icon="account_circle" :value="group" no-action>
+            <template v-slot:activator>
+              <v-list-tile ripple>
+                <v-list-tile-title>Profile</v-list-tile-title>
+              </v-list-tile>
+            </template>
+            <template v-if="!miniVariant">
+              <v-list-tile
+                ripple
+                v-for="link in linkProfile"
+                :key="link.text"
+                router
+                :to="link.route"
+              >
+                <v-list-tile-title>{{link.text}}</v-list-tile-title>
+                <v-list-tile-action>
+                  <v-icon>{{link.icon}}</v-icon>
+                </v-list-tile-action>
+              </v-list-tile>
+            </template>
+          </v-list-group>
+          <!--sub list other-->
+          <v-list-tile ripple v-for="link in links" :key="link.text" router :to="link.route">
+            <v-list-tile-action>
+              <v-icon color="#676767">{{link.icon}}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title class="grey--text">{{link.text}}</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+
+        <v-spacer></v-spacer>
+        <v-divider></v-divider>
+        <v-list dense class="py-0">
+          <v-list-tile v-if="checkProgramCoordinatoship" ripple router to="/coordinator/dashboard">
+            <v-list-tile-action>
+              <v-icon color="#676767">pages</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title class="grey--text">Coordinator Menu</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile v-if="checkProgramMentorship" ripple router to="/mentor/dashboard">
+            <v-list-tile-action>
+              <v-icon color="#676767">pages</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title class="grey--text">Mentor Menu</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-layout>
     </v-navigation-drawer>
 
     <v-navigation-drawer temporary right v-model="rightDrawer" fixed>
@@ -287,25 +305,23 @@ export default {
   color: grey;
   pointer-events: none;
 }
-#navdrawtalent::-webkit-scrollbar-track
-{
-	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
-	border-radius: 10px;
-	background-color: #F5F5F5;
+#navdrawtalent::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  border-radius: 10px;
+  background-color: #f5f5f5;
 }
 
-#navdrawtalent::-webkit-scrollbar
-{
-	width: 5px;
-	background-color: #F5F5F5;
+#navdrawtalent::-webkit-scrollbar {
+  width: 5px;
+  background-color: #f5f5f5;
 }
 
-#navdrawtalent::-webkit-scrollbar-thumb
-{
-	border-radius: 10px;
-	-webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
-	background-color: #a5a5a5;
+#navdrawtalent::-webkit-scrollbar-thumb {
+  border-radius: 10px;
+  box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+  background-color: #a5a5a5;
 }
-
 </style>
 
