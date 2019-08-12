ba59c46b4aa9043a4004c3593b7337b093e964da
Tri S <mas.trisutrisno@gmail.com>
2019-08-12 18:06:35 +0700

    remove datetiime picker

--- src/main.js
+++ src/main.js
@@ -12,8 +12,6 @@ import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css"; // Ensure you are using css-loader
-import DatetimePicker from 'vuetify-datetime-picker'
-import 'vuetify-datetime-picker/src/stylus/main.styl'
 import { TiptapVuetifyPlugin } from "tiptap-vuetify";
 import 'tiptap-vuetify/dist/main.css';
 import VueVisible from "vue-visible";
@@ -45,7 +43,6 @@ Vue.use(Vuetify, {
   },
   theme: customcolors
 });
-Vue.use(DatetimePicker)
 Vue.use(TiptapVuetifyPlugin)
 
 Vue.config.productionTip = false;
