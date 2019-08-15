c6794f077ea5dc94e90e93d48c9aacca3790d23e
Tri S <mas.trisutrisno@gmail.com>
2019-08-15 16:19:10 +0700

    Update Notification.vue

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,22 +1,22 @@
 <template>
   <v-layout align-center justify-center>
     
-    <v-snackbar v-model="status.success" :auto-height="true" :multi-line="true" color="green" top class="ma-4">
+    <v-snackbar v-model="status.success" :multi-line="true" color="green" top class="ma-4">
       {{err_msg.details[0]}}
       <v-btn color="white" text @click="status.success = false">Close</v-btn>
     </v-snackbar>
 
-    <v-snackbar v-model="status.info" :auto-height="true" :multi-line="true" color="blue" top class="ma-4">
+    <v-snackbar v-model="status.info" :multi-line="true" color="blue" top class="ma-4">
       {{err_msg.details[0]}}
       <v-btn color="white" text @click="status.info = false">Close</v-btn>
     </v-snackbar>
 
-    <v-snackbar v-model="status.warning" :auto-height="true" :multi-line="true" color="yellow" top class="ma-4">
+    <v-snackbar v-model="status.warning" :multi-line="true" color="yellow" top class="ma-4">
       {{err_msg.details[0]}}
       <v-btn color="white" text @click="status.warning = false">Close</v-btn>
     </v-snackbar>
 
-    <v-snackbar v-model="status.error" :auto-height="true" :multi-line="true" color="red" top class="ma-4">
+    <v-snackbar v-model="status.error" :multi-line="true" color="red" top class="ma-4">
       {{err_msg.code}} {{err_msg.details[0]}}
       <v-btn color="white" text @click="status.error = false">Close</v-btn>
     </v-snackbar>
