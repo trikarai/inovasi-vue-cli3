2f2faea2d65162f5bf3940c450e469962e5e450a
Tri S <mas.trisutrisno@gmail.com>
2019-08-15 14:51:31 +0700

    fix profile-work data-table

--- src/components/talent/profile/work/TalentProfileWork.vue
+++ src/components/talent/profile/work/TalentProfileWork.vue
@@ -6,25 +6,23 @@
         <v-icon>add</v-icon>
         {{ $vuetify.lang.t('$vuetify.action.add') }} Work Experiences
       </v-btn>
-
+      <!-- {{data.list}} -->
       <v-data-table :headers="headers" :items="data.list" :loading="loader" class="elevation-1 mt-1">
-        <template v-slot:items="props">
-          <td>{{ props.item.companyName }}</td>
-          <td class="text-xs-right">
-            <v-btn @click="openEdit(props.index)" small>
+        <template v-slot:item.action="{ item }">
+            <v-btn @click="openEdit(item.id)" small>
               <v-icon small>edit</v-icon>
               {{ $vuetify.lang.t('$vuetify.action.edit') }}
             </v-btn>
-            <v-btn small dark color="warning" @click="deleteAct(props.index)">
+            <v-btn small dark color="warning" @click="deleteAct(item.id)">
               <v-icon small>delete</v-icon>
               {{ $vuetify.lang.t('$vuetify.action.delete') }}
             </v-btn>
             <v-expand-transition>
-              <div v-show="props.index == selectedIndex">
+              <div v-show="item.id == selectedIndex">
                 <div>
                    <v-icon>warning</v-icon> <span> {{ $vuetify.lang.t('$vuetify.action.confirmationtodelete') }}</span>
                 </div>
-                <v-btn text dark @click="deleteData(props.item.id)" color="red">
+                <v-btn text dark @click="deleteData(item.id)" color="red">
                   <v-icon></v-icon>
                   {{ $vuetify.lang.t('$vuetify.action.yes') }}
                 </v-btn>
@@ -34,7 +32,6 @@
                 </v-btn>
               </div>
             </v-expand-transition>
-          </td>
         </template>
       </v-data-table>
     </v-container>
@@ -84,9 +81,9 @@ export default {
           text: "Company Name",
           align: "left",
           sortable: false,
-          value: "name"
+          value: "companyName"
         },
-        { text: "", value: "id", sortable: false }
+        { text: "", value: "action", sortable: false }
       ]
     };
   },
@@ -117,7 +114,7 @@ export default {
       this.dialogForm = true;
       this.view = false;
       this.edit = true;
-      this.singleData = this.data.list[index];
+      this.singleData.id = index;
     },
     openAdd: function() {
       this.dialogForm = true;
