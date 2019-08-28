b46fc1f8bc56c6ed452f138c9fd63392023f9dc2
Tri S <mas.trisutrisno@gmail.com>
2019-08-28 15:20:41 +0700

    remove version on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
       <!-- <v-card-actions class="grey lighten-3 justify-center"> -->
         <div class="grey--text subtitle-2 ml-auto">
           &copy;2019, <strong>MIKTI - Start</strong> &nbsp;&nbsp;|&nbsp;&nbsp; 
-          <span class="grey--text text--lighten-1">Ver 4.0.27</span>
+          <!-- <span class="grey--text text--lighten-1">Ver 4.0.27</span> -->
         </div>
       <!-- </v-card-actions> -->
     <!-- </v-card> -->
