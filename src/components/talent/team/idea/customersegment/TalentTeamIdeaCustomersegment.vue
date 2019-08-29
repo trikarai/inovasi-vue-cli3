8e5be2f33dc4a9a5012bdfb2df8522de7c8877bd
Tri S <mas.trisutrisno@gmail.com>
2019-08-29 14:43:09 +0700

    Add Colloborator module on CS

--- src/components/talent/team/idea/customersegment/TalentTeamIdeaCustomersegment.vue
+++ src/components/talent/team/idea/customersegment/TalentTeamIdeaCustomersegment.vue
@@ -2,8 +2,9 @@
   <div>
     <v-container>
       <notification-alert ref="notif" v-bind:err_msg="err_msg" v-bind:status="status" />
-      
+
       <loader-dialog v-model="loader"></loader-dialog>
+      <form-collaboration v-model="collaboratorForm" @setUpCollaboration="setUpCollaboration"></form-collaboration>
 
       <v-layout row wrap>
         <v-flex xs12 md6>
@@ -14,15 +15,20 @@
                   <v-list-item-title
                     class="font-weight-light white--text body-2"
                   >{{ $vuetify.lang.t('$vuetify.idea.customersegment') }}</v-list-item-title>
-                  
+
                   <v-list-item-subtitle>
                     <h4 class="headline mb-0 white--text">{{parentData.name}}</h4>
                   </v-list-item-subtitle>
                 </v-list-item-content>
                 <v-list-item-action>
-                  <v-btn small fab @click="openEditParent(parentData.id)" class="ml-2">
-                    <v-icon>edit</v-icon>
-                  </v-btn>
+                  <div>
+                    <v-btn small fab class="ml-2 mt-1" @click="openCollaborator()">
+                      <v-icon>share</v-icon>
+                    </v-btn>
+                    <v-btn small fab @click="openEditParent(parentData.id)" class="ml-2 mt-1">
+                      <v-icon>edit</v-icon>
+                    </v-btn>
+                  </div>
                 </v-list-item-action>
               </v-list-item>
             </v-card>
@@ -30,14 +36,22 @@
             <v-list-item style="padding-left:26px;padding-right:26px">
               <v-list-item-content>
                 <v-list-item-title>Description</v-list-item-title>
-                <v-text class="grey--text font-weight-light">{{parentData.description}}</v-text>
+                <text class="grey--text font-weight-light">{{parentData.description}}</text>
               </v-list-item-content>
             </v-list-item>
 
             <v-card-text class="caption" style="padding-left:26px;padding-right:26px">
-              <b>created time:</b> {{parentData.createdTime | moment("Do MMMM YYYY")}}
+              <b>created time:</b>
+              {{parentData.createdTime | moment("Do MMMM YYYY")}}
             </v-card-text>
 
+            <!-- start collaborator module-->
+            <base-collaboration
+              v-if="collaborators.total != 0"
+              v-bind:collaborators="collaborators"
+              @removeCollaborator="removeCollaborator"
+            />
+            <!-- end collaborator module-->
           </v-card>
         </v-flex>
         <v-flex xs12 md6>
@@ -45,7 +59,9 @@
             <v-card class="taitelcs primary white--text elevation-5">
               <v-list-item>
                 <v-list-item-content>
-                  <h3 class="headline mb-0 font-weight-light white--text">{{ $vuetify.lang.t('$vuetify.idea.persona') }}</h3>
+                  <h3
+                    class="headline mb-0 font-weight-light white--text"
+                  >{{ $vuetify.lang.t('$vuetify.idea.persona') }}</h3>
                 </v-list-item-content>
                 <v-list-item-action>
                   <v-btn small fab @click="openAdd" class="ml-2 mt-1">
@@ -54,79 +70,58 @@
                 </v-list-item-action>
               </v-list-item>
             </v-card>
-          <v-list style="margin:0px 10px 10px 10px">
-            <!-- <v-subheader>Persona</v-subheader> -->
-            <template v-if="data.total != 0">
-              <v-list-item v-for="(item, index) in data.list" :key="item.id">
-                <v-list-item-avatar>
-                  <v-btn text @click="openDetail(item.id)">
-                    <v-icon large>pageview</v-icon>
-                  </v-btn>
-                </v-list-item-avatar>
-                <v-list-item-content>
-                  {{item.name}}
-                </v-list-item-content>
-                <v-list-item-action>
-                  <!-- <v-btn text @click="deleteAct(index)" small>
+            <v-list style="margin:0px 10px 10px 10px">
+              <!-- <v-subheader>Persona</v-subheader> -->
+              <template v-if="data.total != 0">
+                <v-list-item v-for="(item, index) in data.list" :key="item.id">
+                  <v-list-item-avatar>
+                    <v-btn text @click="openDetail(item.id)">
+                      <v-icon large>pageview</v-icon>
+                    </v-btn>
+                  </v-list-item-avatar>
+                  <v-list-item-content>{{item.name}}</v-list-item-content>
+                  <v-list-item-action>
+                    <!-- <v-btn text @click="deleteAct(index)" small>
                     <v-icon color="warning">delete</v-icon>
-                  </v-btn> -->
+                    </v-btn>-->
 
-                  <v-flex>
-                    <v-btn
-                      color="warning"
-                      @click="deleteAct(index)"
-                      v-if="selectedIndex == null"
-                      small
-                    >
-                      <!-- <v-icon>delete</v-icon>  -->
-                      delete
-                    </v-btn>
+                    <v-flex>
+                      <v-btn
+                        color="warning"
+                        @click="deleteAct(index)"
+                        v-if="selectedIndex == null"
+                        small
+                      >
+                        <!-- <v-icon>delete</v-icon>  -->
+                        delete
+                      </v-btn>
 
-                    <v-flex v-show="index == selectedIndex">
-                      <v-icon>warning</v-icon>
-                      <v-text class="caption">{{ $vuetify.lang.t('$vuetify.action.confirmationtodelete') }}</v-text>
-                      <br>
-                      <v-btn small dark @click="deleteData(item.id)" color="red" class="ml-10">
-                        <v-icon></v-icon>
-                        {{ $vuetify.lang.t('$vuetify.action.yes') }}
-                      </v-btn>
-                      <v-btn small text @click="deleteAct(null)" class="ml-2">
-                        <v-icon></v-icon>
-                        {{ $vuetify.lang.t('$vuetify.action.cancel') }}
-                      </v-btn>
+                      <v-flex v-show="index == selectedIndex">
+                        <v-icon>warning</v-icon>
+                        <text
+                          class="caption"
+                        >{{ $vuetify.lang.t('$vuetify.action.confirmationtodelete') }}</text>
+                        <br />
+                        <v-btn small dark @click="deleteData(item.id)" color="red" class="ml-10">
+                          <v-icon></v-icon>
+                          {{ $vuetify.lang.t('$vuetify.action.yes') }}
+                        </v-btn>
+                        <v-btn small text @click="deleteAct(null)" class="ml-2">
+                          <v-icon></v-icon>
+                          {{ $vuetify.lang.t('$vuetify.action.cancel') }}
+                        </v-btn>
+                      </v-flex>
                     </v-flex>
-                  </v-flex>
-                  
-                </v-list-item-action>
-                  
-
-                <!-- <v-scale-transition>
-                  <div v-show="index == selectedIndex">
-                    {{ $vuetify.lang.t('$vuetify.action.confirmationtodelete') }}
-                    <v-btn small @click="deleteData(item.id)" color="red" class="ma-2">
-                      <v-icon></v-icon>
-                      {{ $vuetify.lang.t('$vuetify.action.yes') }}
-                    </v-btn>
-                    <v-btn small @click="deleteAct(null)">
-                      <v-icon></v-icon>
-                      {{ $vuetify.lang.t('$vuetify.action.cancel') }}
-                    </v-btn>
-                  </div>
-                </v-scale-transition> -->
-
-              </v-list-item>
-            </template>
-            <template v-else>
+                  </v-list-item-action>
+                </v-list-item>
+              </template>
+              <template v-else>
                 <p
-                class="text-center"
-                style="margin-top:30px;"
-              >{{ $vuetify.lang.t('$vuetify.noDataText') }}</p>
-            </template>
-          </v-list>
-          <!-- <v-btn color="primary" @click="openAdd" class="ml-3">
-            <v-icon>add</v-icon>
-            {{$vuetify.lang.t("$vuetify.action.add")}}
-          </v-btn> -->
+                  class="text-center"
+                  style="margin-top:30px;"
+                >{{ $vuetify.lang.t('$vuetify.noDataText') }}</p>
+              </template>
+            </v-list>
           </v-card>
         </v-flex>
       </v-layout>
@@ -160,10 +155,15 @@ import Notification from "@/components/Notification";
 import CustomerSegmentForm from "./CustomerSegmentForm";
 import PersonaForm from "./persona/PersonaForm";
 
+import BaseCollaboration from "@/components/talent/team/components/BaseCollaboration";
+import FormCollaboration from "@/components/talent/team/components/CollaboratorForm";
+
 export default {
   components: {
     CustomerSegmentForm,
     PersonaForm,
+    BaseCollaboration,
+    FormCollaboration,
     "notification-alert": Notification
   },
   data() {
@@ -174,9 +174,10 @@ export default {
         info: false,
         warning: false
       },
-      err_msg: {details:[""]},
+      err_msg: { details: [""] },
       error: "error",
       loader: false,
+      collaboratorForm: false,
       dialogDel: false,
       dialogForm: false,
       dialogFormParent: false,
@@ -187,12 +188,14 @@ export default {
       selectedIndex: null,
       data: { total: 0, list: [] },
       parentData: "",
-      singleData: { id: "", name: "" }
+      singleData: { id: "", name: "" },
+      collaborators: { total: 0, list: [] }
     };
   },
   mounted: function() {
     this.getParentData();
     this.getDataList();
+    this.loadCollaborator();
   },
   methods: {
     getParentData: function() {
@@ -208,15 +211,13 @@ export default {
             "/customer-segments/" +
             this.$route.params.customersegmentId
         )
-        .then(
-          res => {
-            this.parentData = res.data.data;
-          }
-        )
-        .catch(error=> {
-            notif.showError(this,error);       
+        .then(res => {
+          this.parentData = res.data.data;
         })
-        .finally(()=> {
+        .catch(error => {
+          notif.showError(this, error);
+        })
+        .finally(() => {
           this.loader = false;
         });
     },
@@ -234,18 +235,17 @@ export default {
             this.$route.params.customersegmentId +
             "/personas"
         )
-        .then(
-          res => {
-            if (res.data.data) {
-              this.data = res.data.data;
-            } else {
-              this.data = {total:0,list:[]}
-            }
+        .then(res => {
+          if (res.data.data) {
+            this.data = res.data.data;
+          } else {
+            this.data = { total: 0, list: [] };
           }
-        ).catch(error=>{
-          notif.showError(this,error);
         })
-        .finally(()=> {
+        .catch(error => {
+          notif.showError(this, error);
+        })
+        .finally(() => {
           this.loader = false;
         });
     },
@@ -281,7 +281,6 @@ export default {
       this.singleData = { id: "", name: "" };
     },
     deleteAct: function(index) {
-      // alert(index)
       if (this.selectedIndex == index) {
         this.selectedIndex = null;
       } else {
@@ -301,11 +300,11 @@ export default {
             "/personas/" +
             id
         )
-        .then(res=>{})
+        .then(res => {})
         .catch(error => {
-          notif.showError(this,error);
+          notif.showError(this, error);
         })
-        .finally(()=> {
+        .finally(() => {
           this.selectedIndex = null;
           this.refresh();
         });
@@ -323,15 +322,11 @@ export default {
             id +
             "/set_as_main_idea"
         )
-        .then(
-          res => {
-            
-          }
-        )
-        .catch(error=> {
-          notif.showError(this,error);
+        .then(res => {})
+        .catch(error => {
+          notif.showError(this, error);
         })
-        .finally(()=> {
+        .finally(() => {
           this.selectedIndex = null;
           this.refresh();
         });
@@ -343,6 +338,84 @@ export default {
     refreshParent: function() {
       this.dialogFormParent = false;
       this.getParentData();
+    },
+    loadCollaborator: function() {
+      this.loader = true;
+      notif.reset(this);
+      net
+        .getData(
+          this,
+          "/talent/as-team-member/" +
+            this.$route.params.teamId +
+            "/ideas/" +
+            this.$route.params.ideaId +
+            "/customer-segments/" +
+            this.$route.params.customersegmentId +
+            "/collaborators"
+        )
+        .then(res => {
+          if (res.data.data) {
+            this.collaborators = res.data.data;
+          } else {
+            this.collaborators = { total: 0, list: [] };
+          }
+        })
+        .catch(error => {
+          notif.showError(this, error);
+        })
+        .finally(() => {
+          this.loader = false;
+        });
+    },
+    openCollaborator: function() {
+      this.collaboratorForm = true;
+    },
+    setUpCollaboration: function(params) {
+      net
+        .putData(
+          this,
+          "/talent/as-team-member/" +
+            this.$route.params.teamId +
+            "/ideas/" +
+            this.$route.params.ideaId +
+            "/customer-segments/" +
+            this.$route.params.customersegmentId +
+            "/collaborators",
+          params
+        )
+        .then(res => {
+          this.collaboratorForm = false;
+          this.loadCollaborator();
+        })
+        .catch(error => {
+          notif.showError(this, error);
+        })
+        .finally(() => {
+          this.collaboratorForm = false;
+        });
+    },
+    removeCollaborator: function(mentorId) {
+      this.loader = true;
+      net
+        .deleteData(
+          this,
+          "/talent/as-team-member/" +
+            this.$route.params.teamId +
+            "/ideas/" +
+            this.$route.params.ideaId +
+            "/customer-segments/" +
+            this.$route.params.customersegmentId +
+            "/collaborators/" + mentorId
+        )
+        .then(res => {
+          this.loadCollaborator();
+        })
+        .catch(error => {
+          notif.showError(this, error);
+        })
+        .finally(() => {
+          this.loader = false;
+        });
     }
   }
 };
