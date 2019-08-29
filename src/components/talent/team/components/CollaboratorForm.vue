fb957bb20e5e92107c168f44dead3def0902e755
Tri S <mas.trisutrisno@gmail.com>
2019-08-29 14:43:33 +0700

    Create Collaborator Form as Component

---
+++ src/components/talent/team/components/CollaboratorForm.vue
@@ -0,0 +1,159 @@
+<template>
+  <v-dialog width="500" v-model="show">
+    <notification-alert ref="notif" v-bind:err_msg="err_msg" v-bind:status="status" />
+    <v-card>
+      <v-card-text>
+        <v-container>
+          <v-form ref="form" v-model="valid">
+            <v-flex>
+              <v-select
+                :loading="programLoader"
+                :disabled="programDisable"
+                v-model="collaboratorParams.programmeId"
+                :items="program.list"
+                item-text="programme.name"
+                item-value="programme.id"
+                :rules="rules"
+                :label="$vuetify.lang.t('$vuetify.team.programParticipation')"
+              ></v-select>
+            </v-flex>
+            <v-flex>
+              <v-autocomplete
+                :loading="mentorLoader"
+                :disabled="mentorDisable"
+                v-model="collaboratorParams.mentorId"
+                :items="mentor.list"
+                item-text="talent.name"
+                item-value="id"
+                :rules="rules"
+                label="Mentor"
+              ></v-autocomplete>
+            </v-flex>
+            <v-layout justify-space-between>
+              <v-btn
+                @click="setUp"
+                block
+                :class=" { 'primary white--text' : valid}"
+                :disabled="!valid"
+                color="primary"
+              >
+                <v-icon left small>share</v-icon>Share
+              </v-btn>
+            </v-layout>
+          </v-form>
+        </v-container>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+<script>
+import net from "@/config/httpclient";
+import notif from "@/config/alerthandling";
+import Notification from "@/components/Notification";
+
+export default {
+  props: {
+    value: Boolean
+  },
+  data() {
+    return {
+      status: {
+        success: false,
+        error: false,
+        info: false,
+        warning: false
+      },
+      err_msg: { details: [""] },
+      valid: false,
+      loadingBtn: false,
+      collaboratorParams: {
+        programmeId: "",
+        mentorId: ""
+      },
+      rules: [v => !!v || "Field is required"],
+      program: {
+        total: 0,
+        list: []
+      },
+      programLoader: false,
+      programDisable: true,
+      mentor: {
+        total: 0,
+        list: []
+      },
+      mentorLoader: false,
+      mentorDisable: true
+    };
+  },
+  components: { "notification-alert": Notification },
+  computed: {
+    show: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("input", value);
+      }
+    }
+  },
+  watch: {
+    "collaboratorParams.programmeId": function() {
+      this.getMentorList();
+    }
+  },
+  mounted: function() {
+    this.getProgramList();
+  },
+  methods: {
+    getProgramList: function() {
+      this.programDisable = true;
+      this.programLoader = true;
+      net
+        .getProgram(this, this.$route.params.teamId)
+        .then(res => {
+          if (res.data.data) {
+            this.program = res.data.data;
+          } else {
+            this.program = { total: 0, list: [] };
+          }
+          this.programDisable = false;
+          this.programLoader = false;
+        })
+        .catch(error => {
+          notif.showError(this, error);
+        })
+        .finally(() => {
+          this.programLoader = false;
+        });
+    },
+    getMentorList: function() {
+      this.mentorLoader = true;
+      this.mentorDisable = true;
+      net
+        .getMentorList(this, this.collaboratorParams.programmeId)
+        .then(res => {
+          if (res.data.data) {
+            this.mentor = res.data.data;
+          } else {
+            this.mentor = { total: 0, list: [] };
+          }
+          this.mentorDisable = false;
+        })
+        .catch(error => {
+          notif.showError(this, error);
+        })
+        .finally(() => {
+          this.mentorLoader = false;
+        });
+    },
+    setUp: function() {
+      if (this.$refs.form.validate()) {
+        this.valid = false;
+        this.$emit("setUpCollaboration", this.collaboratorParams);
+      }
+    }
+  }
+};
+</script>
+<style scoped>
+</style>
