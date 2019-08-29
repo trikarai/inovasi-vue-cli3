c638af73379199ad37083260c7730eeafc0f34eb
Tri S <mas.trisutrisno@gmail.com>
2019-08-29 12:51:01 +0700

    Create BaseCollaboration.vue
    modul to place in every detail entity to show collaborator list

---
+++ src/components/talent/team/components/BaseCollaboration.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-container>
+    <v-divider></v-divider>
+    <v-card-title>{{ $vuetify.lang.t('$vuetify.collaboration.collaboration') }}</v-card-title>
+    <v-data-table
+      v-show="collaborators.list"
+      :headers="collaboratorHeaders"
+      :items="collaborators.list"
+    >
+      <template v-slot:item.action="{item}">
+        <v-btn
+          @click="removeCollaborator(item.mentor.id)"
+          small
+          color="warning"
+        >{{ $vuetify.lang.t('$vuetify.action.cancel') }}</v-btn>
+      </template>
+    </v-data-table>
+  </v-container>
+</template>
+<script>
+export default {
+  props: ["collaborators"],
+  data() {
+    return {
+      collaboratorHeaders: [
+        {
+          text: "Mentor Name",
+          value: "mentor.talent.name",
+          sortable: false,
+          align: "left"
+        },
+        { text: "", value: "action", sortable: false, align: "right" }
+      ]
+    };
+  },
+  methods: {
+      removeCollaborator: function(mentorId){
+          this.$emit("removeCollaborator", mentorId);
+      }
+  }
+};
+</script>
+<style scoped>
+</style>
