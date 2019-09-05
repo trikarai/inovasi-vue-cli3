84bff4c0187b59b721b48cdc69063d52667d8710
Tri S <mas.trisutrisno@gmail.com>
2019-09-05 15:36:20 +0700

    Update treeMixins.js

--- src/mixins/treeMixins.js
+++ src/mixins/treeMixins.js
@@ -56,6 +56,87 @@ export const treeMixins = {
                 }]
             }];
             return data;
+        },
+        canvasTree: function (item) {
+            var data = [{
+                id: item.businessCanvas.id,
+                name: item.businessCanvas.form.name,
+                children: [{
+                    id: item.businessCanvas.valueProposition.id,
+                    name: item.businessCanvas.valueProposition.description,
+                    children: [{
+                        id: item.businessCanvas.valueProposition.persona.id,
+                        name: item.businessCanvas.valueProposition.persona.name,
+                        children: [
+                            {
+                                id: item.businessCanvas.valueProposition.persona.customerSegment.id,
+                                name: item.businessCanvas.valueProposition.persona.customerSegment.name,
+                                children: [
+                                    {
+                                        id: item.businessCanvas.valueProposition.persona.customerSegment.idea.id,
+                                        name: item.businessCanvas.valueProposition.persona.customerSegment.idea.name
+                                    }
+                                ]
+                            }
+                        ]
+                    }]
+                }]
+            }];
+            return data;
+        },
+        expTree: function (item) {
+            var data = [{
+                id: item.experiment.id,
+                name: item.experiment.form.name,
+                children: [{
+                    id: item.experiment.valueProposition.id,
+                    name: item.experiment.valueProposition.description,
+                    children: [{
+                        id: item.experiment.valueProposition.persona.id,
+                        name: item.experiment.valueProposition.persona.name,
+                        children: [
+                            {
+                                id: item.experiment.valueProposition.persona.customerSegment.id,
+                                name: item.experiment.valueProposition.persona.customerSegment.name,
+                                children: [
+                                    {
+                                        id: item.experiment.valueProposition.persona.customerSegment.idea.id,
+                                        name: item.experiment.valueProposition.persona.customerSegment.idea.name
+                                    }
+                                ]
+                            }
+                        ]
+                    }]
+                }]
+            }];
+            return data;
+        },
+        competitorTree: function (item) {
+            var data = [{
+                id: item.competitor.id,
+                name: item.competitor.name,
+                children: [{
+                    id: item.competitor.valueProposition.id,
+                    name: item.competitor.valueProposition.description,
+                    children: [{
+                        id: item.competitor.valueProposition.persona.id,
+                        name: item.competitor.valueProposition.persona.name,
+                        children: [
+                            {
+                                id: item.competitor.valueProposition.persona.customerSegment.id,
+                                name: item.competitor.valueProposition.persona.customerSegment.name,
+                                children: [
+                                    {
+                                        id: item.competitor.valueProposition.persona.customerSegment.idea.id,
+                                        name: item.competitor.valueProposition.persona.customerSegment.idea.name
+                                    }
+                                ]
+                            }
+                        ]
+                    }]
+                }]
+            }];
+            return data;
         }
     }
 }
