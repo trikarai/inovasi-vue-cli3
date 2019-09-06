6a90c90c8410108e6de24be3af70f34f04e44d8a
Arief Maulana <ariefmaualan2014@gmail.com>
2019-09-06 18:30:59 +0700

    UI  |  Comment

--- src/components/talent/team/components/BaseComment.vue
+++ src/components/talent/team/components/BaseComment.vue
@@ -1,33 +1,38 @@
 <template>
+<div>
   <v-container>
     <v-divider></v-divider>
     <v-card-title class="subtitle-1">{{ $vuetify.lang.t('$vuetify.feedback.feedback') }}</v-card-title>
-    <v-timeline dense clipped>
+    <v-timeline class="pr-5" style="height:450px;overflow:auto;" dense clipped>
       <v-timeline-item fill-dot color="omikti" large>
         <template v-slot:icon>
-          <v-icon>add_comment</v-icon>
+          <v-icon class="mt-1" style="color:#fff;">add_comment</v-icon>
         </template>
-        <v-textarea hide-details flat label="Leave a comment..." solo>
+        <v-textarea class="mr-0" rows="2" hide-details flat label="Leave a comment..." solo>
           <template v-slot:append>
             <v-btn class="mx-0" depressed>{{ $vuetify.lang.t('$vuetify.feedback.post') }}</v-btn>
           </template>
         </v-textarea>
       </v-timeline-item>
-      <v-timeline-item v-for="n in 4" :key="n" small>
-        <template v-slot:icon>
+      <v-timeline-item v-for="n in 4" :key="n" color="primary" small>
+        <!-- <template v-slot:icon>
           <v-avatar>
             <img src="http://i.pravatar.cc/64" />
           </v-avatar>
-        </template>
-        <v-card class="elevation-2">
-          <v-card-title class="headline">Mentor</v-card-title>
-          <v-card-text>12 Agustus 2019</v-card-text>
+        </template> -->
+        <v-card class="elevation-3 pa-3">
+          <v-card-title class="title">Mentor</v-card-title>
+          <v-card-text class="caption"><v-icon class="mb-1 mr-1" color="primary" small>calendar_today</v-icon> 12 Agustus 2019</v-card-text>
           <v-card-text>Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.</v-card-text>
-          <v-card-actions><v-spacer/> <v-btn fab small><v-icon small>reply</v-icon></v-btn> <v-btn color="warning" fab small><v-icon small>delete</v-icon></v-btn> </v-card-actions>
+          <v-card-actions><v-spacer/> 
+            <v-btn depressed small><v-icon small>reply</v-icon></v-btn>
+            <v-btn color="warning" depressed small><v-icon small>delete</v-icon></v-btn> 
+          </v-card-actions>
         </v-card>
       </v-timeline-item>
     </v-timeline>
   </v-container>
+</div>
 </template>
 <script>
 export default {
