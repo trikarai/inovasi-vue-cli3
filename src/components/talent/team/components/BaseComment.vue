dfe154732c6a2043e76eb3518e5960328ba20abc
Tri S <mas.trisutrisno@gmail.com>
2019-09-09 11:04:39 +0700

    fix timeline divider on comment

--- src/components/talent/team/components/BaseComment.vue
+++ src/components/talent/team/components/BaseComment.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-    <v-container>
+    <v-container style="height:450px;overflow:auto;">
       <!-- {{comments.list}} -->
       <v-divider></v-divider>
       <v-card-title class="subtitle-1">{{ $vuetify.lang.t('$vuetify.feedback.feedback') }}</v-card-title>
-      <v-timeline class="pr-5" style="height:450px;overflow:auto;" dense clipped>
+      <v-timeline class="pr-5" dense clipped>
         <v-timeline-item fill-dot color="omikti" large>
           <template v-slot:icon>
             <v-icon class="mt-1" style="color:#fff;">add_comment</v-icon>
@@ -36,7 +36,9 @@
               {{comment.submitTime}}
             </v-card-text>
             <v-card-text class="title">{{comment.content}}</v-card-text>
-            <v-card color="grey" v-if="comment.parent != null"><blockquote class="blockquote caption">{{comment.parent.content}}</blockquote></v-card>
+            <v-card color="grey" v-if="comment.parent != null">
+              <blockquote class="blockquote caption">{{comment.parent.content}}</blockquote>
+            </v-card>
             <v-card-actions>
               <v-spacer />
               <v-btn small @click="replyAct(comment.id)" v-if="selectedComment == null">
