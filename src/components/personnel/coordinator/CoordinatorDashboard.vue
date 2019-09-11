431a13a31f17aa75791a713867c874ca6b8f832c
Tri S <mas.trisutrisno@gmail.com>
2019-09-11 08:54:54 +0700

    Update CoordinatorDashboard.vue

--- src/components/personnel/coordinator/CoordinatorDashboard.vue
+++ src/components/personnel/coordinator/CoordinatorDashboard.vue
@@ -1,11 +1,32 @@
 <template>
   <v-container>
     <v-layout>
-      Dashboard Coordinator
+      <v-flex xs12 md5>
+        <v-select
+          :items="user.data.programmeCoordinatorship"
+          item-text="programme.name"
+          item-value="id"
+          :no-data-text="$vuetify.lang.t('$vuetify.noDataText')"
+          v-model="programId"
+          :label="$vuetify.lang.t('$vuetify.program.program')"
+          outlined
+          class="pr-3"
+        ></v-select>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
 <script>
-  export default {
+import auth from "@/config/auth";
+export default {
+  data: function() {
+    return {
+      user: "",
+      programId: ""
+    };
+  },
+  created: function() {
+    this.user = JSON.parse(auth.getAuthData());
   }
+};
 </script>
